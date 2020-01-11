3538d1253753b8e684e03ea3c62498c1dab783f7
Daniel Reina <dannyflatiron@gmail.com>
2020-01-11 01:22:15 +0000

    created call and list_restaurants methods. Creating logic for user interface with CLI

--- lib/romantic_restaurants.rb
+++ lib/romantic_restaurants.rb
@@ -3,11 +3,11 @@ require 'nokogiri'
 require 'pry'
 
 
-require "romantic_restaurants/version"
+require_relative "romantic_restaurants/version"
 require "romantic_restaurants/cli"
 require "romantic_restaurants/scraper"
 
-module RomanticRestaurants
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module RomanticRestaurants
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
--- lib/romantic_restaurants/cli.rb
+++ lib/romantic_restaurants/cli.rb
@@ -1,7 +1,18 @@
+#CLI Controller
+
 class RomanticRestaurants::CLI
   
   def call
     puts "Restaurant list"
+    list_restaurants
+  end
+  
+  def list_restaurants
+    puts <<-DOC.gsub /^\s*/, ''
+      1. River Cafe
+      2. Le Bernadine
+    DOC
+  end
   end
   
 end
