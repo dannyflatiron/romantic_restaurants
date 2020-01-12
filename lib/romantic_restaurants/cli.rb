d2a99f65b0e3f0da524217ddb16b8eb34ee56df1
Daniel Reina <dannyflatiron@gmail.com>
2020-01-12 01:21:33 +0000

    added methods goodbye and contact_information and added those methods to the call method

--- lib/romantic_restaurants/cli.rb
+++ lib/romantic_restaurants/cli.rb
@@ -5,6 +5,8 @@ class RomanticRestaurants::CLI
   def call
     list_restaurants
     restaurant_description
+    contact_information
+    goodbye
   end
   
   def list_restaurants
@@ -20,18 +22,38 @@ class RomanticRestaurants::CLI
     puts "Enter the number of the restaurant that you'd like more info on or type exit."
     while input != "exit"
     input = gets.strip.downcase
-    case input
-    when "1"
-      puts "More info on restaurant 1..."
-    when "2"
-      puts "More info on restaurant 2..."
-    when "3"
-      puts "More info on restaurant 3..."
+      case input
+      when "1"
+        puts "More info on restaurant 1..."
+      when "2"
+        puts "More info on restaurant 2..."
+      when "3"
+        puts "More info on restaurant 3..."
+      end
     end
   end
   
   def contact_information
-    puts "
+    puts "Enter number to get restaurant's contact information"
+    while input != "exit"
+    input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Contact information on 1..."
+      when "2"
+        puts "Contact information on 2..."
+      when "3"
+        puts "Contact informaiton on 3..."
+      when "4"
+        puts "Contact information on 4..."
+      end
+    end
+    
+    def goodbye
+      puts "See you later."
+      
+    end
+    
   end
   
 end
