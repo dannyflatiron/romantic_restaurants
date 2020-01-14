308a80b5009870144b3a7579d1583e26251a6902
Daniel Reina <dannyflatiron@gmail.com>
2020-01-14 03:05:10 +0000

    added scrape_restaurants method to restaurants.rb

--- lib/romantic_restaurants/restaurants.rb
+++ lib/romantic_restaurants/restaurants.rb
@@ -1,27 +1,22 @@
+require 'pry'
 class RomanticRestaurants::Restaurant
   
   attr_accessor :name, :phone_number, :address, :url, :description
   
   def self.all
-    # should return a bunch of instances of deals
-    # puts <<-DOC.gsub /^\s*/, ''
-    #   1. River Cafe
-    #   2. Le Bernadine
-    # DOC
-    restaurant_1 = self.new
-    restaurant_1.name = "Le Bernadine"
-    restaurant_1.phone_number = "917_327-2845"
-    restaurant_1.description = "Restaurant description"
-    restaurant_1.address = "2361 Nostrand ave"
-    restaurant_1.url = "https://www.townandcountrymag.com/leisure/dining/g3177/most-romantic-restaurants-in-new-york-city/"
+    self.scrape_restaurants
+  end
+  
+  def self.scrape_restaurants
+    restaurants = []
     
-    restaurant_2 = self.new
-    restaurant_2.name = "River Cafe"
-    restaurant_2.phone_number = "718-337-1804"
-    restaurant_2.description = "Restaurant description"
-    restaurant_2.address = "2222 cornaga ave"
-    restaurant_2.url = "https://www.townandcountrymag.com/leisure/dining/g3177/most-romantic-restaurants-in-new-york-city/"
-    [restaurant_1, restaurant_2]
+    restaurants << self
+    doc = Nokogiri::HTML(open("https://www.townandcountrymag.com/leisure/dining/g3177/most-romantic-restaurants-in-new-york-city/"))
+    binding.pry
+    
+    
+    
+    restaurants
   end
     
   
