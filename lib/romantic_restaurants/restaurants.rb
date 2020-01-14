194996f5545af855dc9e4f9c3a1c978f5e8faea7
Daniel Reina <dannyflatiron@gmail.com>
2020-01-14 04:57:24 +0000

    solved how to access the name of the restaurant

--- lib/romantic_restaurants/restaurants.rb
+++ lib/romantic_restaurants/restaurants.rb
@@ -15,11 +15,18 @@ class RomanticRestaurants::Restaurant
   
   def self.town_and_country
     doc = Nokogiri::HTML(open("https://www.townandcountrymag.com/leisure/dining/g3177/most-romantic-restaurants-in-new-york-city/"))
+    name = doc.search("span.listicle-slide-hed-text").text
     
+    name = doc.css('.listicle-slide-hed').collect do |title|
+      title.css('span')[1].text
+    end
+    end
+
+    binding.pry
+
     restaurant = self.new
-    restaurant.name = doc.search("").text
+    restaurant.name = doc.search("span.listicle-slide-hed-text").first.text
     restaurant.description = doc.search("").text
-    
     restaurants
   end
     
