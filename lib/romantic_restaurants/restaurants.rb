0c8e79c4cae7c845fd402957b82d861c03794d36
Daniel Reina <dannyflatiron@gmail.com>
2020-01-15 02:14:09 +0000

    added notes in restaurant.rb

--- lib/romantic_restaurants/restaurants.rb
+++ lib/romantic_restaurants/restaurants.rb
@@ -2,11 +2,13 @@ require 'pry'
 class RomanticRestaurants::Restaurant
   
   attr_accessor :name, :description
+  # this is unnecessary 
   
   def self.all
     self.scrape_restaurants
   end
   
+  # this should be called self.all
   def self.scrape_restaurants
     restaurants = nil
     
@@ -16,14 +18,19 @@ class RomanticRestaurants::Restaurant
 
   end
   
+  # this should be moved to a different file called RomanticRestaurants::Scraper
+  # method should be called scrape_restaurants
+  
   def self.town_and_country
+    # should be called def self.scrape_webpage
     doc = Nokogiri::HTML(open("https://www.townandcountrymag.com/leisure/dining/g3177/most-romantic-restaurants-in-new-york-city/"))
     
     restaurant_name = doc.css('.listicle-slide-hed').collect{|title| title.css('span')[1].text.strip}
     restaurant_description = doc.css('.listicle-slide-dek').collect{|t| t.text.strip}
       
            
-
+    # this can possibly be stored in a different method called # create_restaurant
+    # this should stay in this class doc called restaurants.rb
     restaurant = self.new
     restaurant.name = restaurant_name
     restaurant.description = restaurant_description
