c8f28dad5568e618fa9b48b1fdc6b5f74b08406a
Daniel Reina <dannyflatiron@gmail.com>
2020-01-16 20:44:40 +0000

    added the file path for scraper to my environment file

--- lib/romantic_restaurants.rb
+++ lib/romantic_restaurants.rb
@@ -8,6 +8,7 @@ require 'pry'
 require_relative "romantic_restaurants/version"
 require_relative "romantic_restaurants/cli"
 require_relative "romantic_restaurants/restaurants"
+require_relative "romantic_restaurants/scraper.rb"
 
 # module RomanticRestaurants
 #   class Error < StandardError; end
