d8fef9270a347dc2c8d0863bd2f1721d04472f7f
Daniel Reina <dannyflatiron@gmail.com>
2020-01-16 21:24:20 +0000

    commented out a lot of methods in cli.rb

--- lib/romantic_restaurants/cli.rb
+++ lib/romantic_restaurants/cli.rb
@@ -3,46 +3,46 @@ require 'pry'
 class RomanticRestaurants::CLI
   
   def call
-    list_restaurants
-    restaurant_description 
+    # list_restaurants
+    # restaurant_description 
     
   end
   
-  def list_restaurants
-    puts "Restaurant list"
-    puts ""
-    puts "----------------------------------"
-    puts ""
-    @restaurant = RomanticRestaurants::Restaurant.all
-    # @restaurant.each {|r| r.name}
-    @restaurant.name.each.with_index(1) do |restaurant, r|
-      puts "#{r}. #{restaurant}"
-    end
-    puts ""
-  end
+  # def list_restaurants
+  #   puts "Restaurant list"
+  #   puts ""
+  #   puts "----------------------------------"
+  #   puts ""
+  #   @restaurant = RomanticRestaurants::Restaurant.all
+  #   # @restaurant.each {|r| r.name}
+  #   @restaurant.name.each.with_index(1) do |restaurant, r|
+  #     puts "#{r}. #{restaurant}"
+  #   end
+  #   puts ""
+  # end
   
-  def restaurant_description
-    input = nil
-    while input != "exit"
-    puts "Enter the number of the restaurant that you'd like more info on or type list to see the restaurants again or type exit."
-    input = gets.strip.downcase
+  # def restaurant_description
+  #   input = nil
+  #   while input != "exit"
+  #   puts "Enter the number of the restaurant that you'd like more info on or type list to see the restaurants again or type exit."
+  #   input = gets.strip.downcase
     
-      if input.to_i > 0
-        # -1 because I want the array index 
-        restaurant = @restaurant.description[input.to_i-1]
+  #     if input.to_i > 0
+  #       # -1 because I want the array index 
+  #       restaurant = @restaurant.description[input.to_i-1]
         
-        puts "#{restaurant}"
-        # binding.pry
+  #       puts "#{restaurant}"
+  #       # binding.pry
         
-        elsif input == "list"
-          list_restaurants
-        elsif input == "exit"
-          goodbye
-        else
-          puts "Note sure what you want, type list or exit."
-      end
-    end
-  end
+  #       elsif input == "list"
+  #         list_restaurants
+  #       elsif input == "exit"
+  #         goodbye
+  #       else
+  #         puts "Note sure what you want, type list or exit."
+  #     end
+  #   end
+  # end
   
   # def contact_information
   #   puts "Enter number to get restaurant's contact information"
