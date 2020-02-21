0bf3c52df2de42474792e06bf9ba9cb12660ec89
Daniel Reina <dannyflatiron@gmail.com>
2020-02-21 00:39:14 +0000

    uncommented lines 35-37

--- lib/romantic_restaurants/restaurants.rb
+++ lib/romantic_restaurants/restaurants.rb
@@ -1,44 +1,44 @@
 require 'pry'
 class RomanticRestaurants::Restaurant
-  
+
   attr_accessor :name, :description
-  # this is unnecessary 
-  
+  # this is unnecessary
+
   def self.all
     self.scrape_restaurants
   end
-  
+
   # this should be called self.all
   def self.scrape_restaurants
     restaurants = nil
-    
+
     restaurants = self.town_and_country
     restaurants
         # binding.pry
   end
-    
-   
-  
+
+
+
   # this should be moved to a different file called RomanticRestaurants::Scraper
   # method should be called scrape_restaurants
-  
+
   def self.town_and_country
     # should be called def self.scrape_webpage
     doc = Nokogiri::HTML(open("https://www.townandcountrymag.com/leisure/dining/g3177/most-romantic-restaurants-in-new-york-city/"))
-    
+
     restaurant_name = doc.css('.listicle-slide-hed').collect{|title| title.css('span')[1].text.strip}
     restaurant_description = doc.css('.listicle-slide-dek').collect{|t| t.text.strip}
-      
-           
+
+
     # this can possibly be stored in a different method called # create_restaurant
     # this should stay in this class doc called restaurants.rb
-    # restaurant = self.new
-    # restaurant.name = restaurant_name
-    # restaurant.description = restaurant_description
+    restaurant = self.new
+    restaurant.name = restaurant_name
+    restaurant.description = restaurant_description
     # # binding.pry
     restaurant
   end
-    
-  
-  
+
+
+
 end
